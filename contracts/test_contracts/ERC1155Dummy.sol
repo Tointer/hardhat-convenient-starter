e65eaa462946ebdac126dc9296f43a5a6f0220f8
ibragimov.i <Tointer@yandex.ru>
2022-07-18 14:01:41 +0300

    test contracts refactor

--- contracts/test_contracts/ERC1155Dummy.sol
+++ contracts/test_contracts/ERC1155Dummy.sol
@@ -5,7 +5,7 @@ import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Burnable.sol";
 
 /**
- * @dev dummy ERC1155 contract with public mint function
+ * @dev dummy ERC1155 contract with public mint functions
  */
 contract ERC1155Dummy is ERC1155, ERC1155Burnable {
     constructor() ERC1155("") {}
--- contracts/test_contracts/ERC721Dummy.sol
+++ contracts/test_contracts/ERC721Dummy.sol
@@ -1,12 +1,14 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.2;
+pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 
-contract ERC721Dummy is ERC721, ERC721Enumerable, Ownable {
+/**
+ * @dev dummy enumerable ERC721 contract with public mint function
+ */
+contract ERC721Dummy is ERC721, ERC721Enumerable {
     using Counters for Counters.Counter;
 
     Counters.Counter private _tokenIdCounter;
